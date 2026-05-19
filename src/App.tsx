3fbafcde768b75c36d00e7d756f5b428a3476072
omarbougarne <omarbougarne98@gmail.com>
2026-05-19 21:15:05 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,23 @@ const INTERVAL_IN_MILISECONDS = 100;
 function App() {
     const[time, setTime] = useState(TIME_IN_MILISECONDS_TO_COUNTDOWN);
 
-useEffect(() => {
-        let interval;
-        const countDownUntilZero = () => {
-            setTime(prevTime => {
-                if (prevTime === 0) clearInterval(interval);
-                else return prevTime - INTERVAL_IN_MILISECONDS;
-            })
-        }
-        interval = setInterval(countDownUntilZero, INTERVAL_IN_MILISECONDS);
-        return () => clearInterval(interval);
-    }, []);
+            let interval;
+            const countDownUntilZero = () => {
+                setTime(prevTime => {
+                    if (prevTime === 0) clearInterval(interval);
+                    else return prevTime - INTERVAL_IN_MILISECONDS;
+                })
+                interval = setInterval(countDownUntilZero, INTERVAL_IN_MILISECONDS);
+                return () => clearInterval(interval);
+            }
+
     const minutes = Math.floor(time / 1000 / 60);
-const seconds = Math.floor((time / 1000) % 60);
+    const seconds = Math.floor((time / 1000) % 60);
   return (
     <>
   {String(minutes).padStart(2, "0")}:
   {String(seconds).padStart(2, "0")}
+  <button onClick={countDownUntilZero}>Start</button>
 </>
   )
 }
